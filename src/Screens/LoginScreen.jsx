753ea11a805c2cb7deb1d0ea2b5d7feba0ebc7a3
Manuhe <dev.by.manda@gmail.com>
2025-02-03 14:08:37 +0300

    Add login screen Navbar
    Added the login screen navbar and also added a cool gradient background image for the whole login screen.

--- src/Screens/LoginScreen.jsx
+++ src/Screens/LoginScreen.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import '../Styles/loginScreen.css';
+import NetflixLogo from '../assets/Netflix_Logo_RGB.png';
+import LoginScreenBackground from '../assets/loginScreenBackground.jpg';
 
 const LoginScreen = () => {
   return (
-    <div className="loginScreen">
-      <h1>Test--login--screen</h1>
+    <div
+      className="loginScreen"
+      style={{
+        backgroundImage: `url(${LoginScreenBackground})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+      }}
+    >
+      <div className="loginScreen__background">
+        <img className="loginScreen__logo" src={NetflixLogo} alt="" />
+        <button className="loginScreen__button">Sign in</button>
+        <div className="loginScreen--gradient"></div>
+      </div>
     </div>
   );
 };
