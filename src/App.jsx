ea2f06f2250d1275b7e7c1941446e635c3e6967c
Manuhe <dev.by.manda@gmail.com>
2025-02-03 13:13:26 +0300

    Add Login Screen
    Add Login Screen and also CSS file, and adding it to App.jsx for rendering it with some logic for showing it if there is no User.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import HomeScreen from './Screens/HomeScreen';
 import './Styles/app.css';
+import LoginScreen from './Screens/LoginScreen';
 
 function App() {
+  const user = null;
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
       </BrowserRouter>
     </div>
   );
---
+++ src/Screens/LoginScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/loginScreen.css';
+
+const LoginScreen = () => {
+  return (
+    <div className="loginScreen">
+      <h1>Test login screen</h1>
+    </div>
+  );
+};
+
+export default LoginScreen;
