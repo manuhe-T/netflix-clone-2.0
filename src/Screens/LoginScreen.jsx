880c6ee4023041165debc55e51561a3dc95d4dbc
Manuhe <dev.by.manda@gmail.com>
2025-02-03 14:45:27 +0300

    Add Login Screen Body
    Add login screen body with beautiful CSS styling to match the Netflix login screen.

--- src/Screens/LoginScreen.jsx
+++ src/Screens/LoginScreen.jsx
@@ -10,6 +10,7 @@ const LoginScreen = () => {
       style={{
         backgroundImage: `url(${LoginScreenBackground})`,
         backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center center',
       }}
     >
@@ -17,6 +18,23 @@ const LoginScreen = () => {
         <img className="loginScreen__logo" src={NetflixLogo} alt="" />
         <button className="loginScreen__button">Sign in</button>
         <div className="loginScreen--gradient"></div>
+      </div>
+
+      <div className="loginScreen__body">
+        <>
+          <h1>Unlimited films, Tv programs and more.</h1>
+          <h2>Watch anywhere, cancel at any time.</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+          <div className="loginScreen__input">
+            <form>
+              <input type="email" placeholder="Email Address" />
+              <button className="loginScreen__getStarted">GET STARTED</button>
+            </form>
+          </div>
+        </>
       </div>
     </div>
   );
