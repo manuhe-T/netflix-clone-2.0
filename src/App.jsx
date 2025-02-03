81220cb958ba15e097dea1d826056644c29e9b50
Manuhe <dev.by.manda@gmail.com>
2025-02-03 13:00:09 +0300

    Rename the Pages folder to Screens folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
-import HomeScreen from './Pages/HomeScreen';
+import HomeScreen from './Screens/HomeScreen';
 import './Styles/app.css';
 
 function App() {
