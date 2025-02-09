3a3ad4a614d41c3ff1bf8cb105486c5f782bc6bb
Manuhe <dev.by.manda@gmail.com>
2025-02-09 10:57:07 +0300

    Add prop-types
    PropTypes is imported from the prop-types package.
    Rows.propTypes is used to specify that title and fetchUrl are required strings, and isLargeRow is an optional boolean.

--- src/Components/Rows.jsx
+++ src/Components/Rows.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import axios from '../axios';
 import '../Styles/row.css';
 
@@ -6,7 +7,7 @@ const Rows = ({ title, fetchUrl, isLargeRow = false }) => {
   const baseUrl = 'https://image.tmdb.org/t/p/original/';
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    async function fetchData(params) {
+    async function fetchData() {
       const requests = await axios.get(fetchUrl);
       setMovies(requests.data.results);
 
@@ -37,6 +38,12 @@ const Rows = ({ title, fetchUrl, isLargeRow = false }) => {
       </div>
     </div>
   );
+};
+
+Rows.propTypes = {
+  title: PropTypes.string.isRequired,
+  fetchUrl: PropTypes.string.isRequired,
+  isLargeRow: PropTypes.bool,
 };
 
 export default Rows;
